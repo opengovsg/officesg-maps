e0f696755ed517dc163bf0eb818e9a433944ac60
datle <thanhdat.toby@gmail.com>
2023-01-20 07:53:10 +0800

    feat: update map.json

---
+++ scripts/push-change.sh
@@ -0,0 +1,4 @@
+git pull
+git add .
+git commit -am "feat: update map.json"
+git push
---
+++ scripts/validate-map.js
@@ -0,0 +1,18 @@
+// import mapGuards_1 from "wa-map-optimizer/dist/guards/mapGuards";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+  return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+const mapGuards_1 = require("wa-map-optimizer/dist/guards/mapGuards");
+const fs_1 = __importDefault(require("fs"));
+
+function isMapFile(filePath) {
+  if (!filePath.endsWith(".json") && !filePath.endsWith(".tmj")) {
+    return false;
+  }
+  const object = JSON.parse(fs_1.default.readFileSync(filePath).toString());
+
+  const parsedMap = mapGuards_1.isMap.safeParse(object)
+  return parsedMap.success;
+}
+
+console.log(isMapFile('../maps/map.json'))
