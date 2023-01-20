ed85617e9237288c052c6bab67e9298293827b9d
Nat <nataliemaetan@gmail.com>
2023-01-20 23:58:37 +0800

    feat: update map.json

--- maps/OfficeFloorplan.tsx
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Floorplan" tilewidth="1878" tileheight="808" tilecount="1" columns="0">
- <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
-  <image width="1878" height="808" source="../../../Downloads/Screenshot 2022-12-27 at 1.23.55 PM.png"/>
- </tile>
-</tileset>
--- maps/Phonebooth.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Phonebooth" tilewidth="32" tileheight="32" tilecount="8" columns="2">
- <image source="../../OGP Office Pixel tiles/Phonebooth.png" width="64" height="128"/>
-</tileset>
