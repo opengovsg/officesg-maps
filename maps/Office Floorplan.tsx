079096bec7f4a90394271e8600469681f0e526a3
Nat <nataliemaetan@gmail.com>
2023-01-20 18:00:52 +0800

    feat: update map.json

--- maps/Office Floorplan.tsx
+++ maps/Office Floorplan.tsx
@@ -1,9 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Floorplan" tilewidth="2698" tileheight="1364" tilecount="3" columns="0">
+<tileset version="1.9" tiledversion="1.9.2" name="Floorplan" tilewidth="2698" tileheight="1364" tilecount="2" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
-  <image width="1878" height="808" source="../../../Downloads/Screenshot 2022-12-27 at 1.23.55 PM.png"/>
- </tile>
  <tile id="1">
   <image width="2698" height="1364" source="../../Floorplans/Screenshot 2023-01-16 at 3.33.47 PM.png"/>
  </tile>
